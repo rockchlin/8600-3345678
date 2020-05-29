f340ca110c422e4b0c49dbdd7a37d317062ee41b
Chi-He Lin <39946556+rockchlin@users.noreply.github.com>
2020-05-29 12:44:49 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
+    if(mySrc === 'https://i.pinimg.com/originals/59/54/b4/5954b408c66525ad932faa693a647e3f.jpg') {
       myImage.setAttribute ('src','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRUqDhsNTvNzHrUvzDdD6cLxV0k5mq6wwxp7jebvHwm3XNb7c-R&usqp=CAU');
     } else {
       myImage.setAttribute ('src','https://i.pinimg.com/originals/59/54/b4/5954b408c66525ad932faa693a647e3f.jpg');
@@ -14,12 +14,12 @@ var myButton= document.querySelector('button');
 var myHeading=document.querySelector('h1');
 
 function setUserName(){
-	let myName= prompt('please enter your name.');
+	let myName= prompt('先生/小姐 你誰?');
 	if (!myName || myName===null){
 		setUserName();
 	}else{
 		localStorage.setItem('name',myName);
-		myHeading.innerHTML='Mozilla is cool,'+myName;
+		myHeading.innerHTML='嗨 你好阿,'+myName;
 		
 		
 	}
